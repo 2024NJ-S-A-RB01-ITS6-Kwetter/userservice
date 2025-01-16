0db0f5eb8fd5b9e74ac3cd8cbd5e4d7b19b4885b
Björn Wildeman <bwildeman99@gmail.com>
2025-01-16 18:31:07 +0100

    Maybe Async fix

--- src/main/java/s_a_rb01_its6/userservice/config/SecurityConfig.java
+++ src/main/java/s_a_rb01_its6/userservice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +18,7 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @RequiredArgsConstructor
 @EnableMethodSecurity(prePostEnabled = true)
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@EnableAsync
 public class SecurityConfig {
 
     private final CustomJwtAuthenticationConverter jwtAuthConverter;
